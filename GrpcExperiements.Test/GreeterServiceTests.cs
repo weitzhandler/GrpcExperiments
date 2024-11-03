c78471d913d7e1d892e9a49b595cc284bd0eac30
Shimmy <2716316+weitzhandler@users.noreply.github.com>
2024-11-03 05:57:13 +0000

    Added logging

--- GrpcExperiements.Test/GreeterServiceTests.cs
+++ GrpcExperiements.Test/GreeterServiceTests.cs
@@ -1,15 +1,27 @@
 using Grpc.Net.Client;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using ProtoBuf.Grpc.Client;
 using ProtoBuf.Grpc.ClientFactory;
 using Shared.Contracts;
+using Xunit.Abstractions;
 using Gncf = Grpc.Net.ClientFactory;
 
 namespace GrpcExperiements.Test;
 
 public class GreeterServiceTests
 {
+    private readonly ILoggerFactory _loggerFactory;
+
+    public GreeterServiceTests(ITestOutputHelper testOutputHelper)
+    {
+        _loggerFactory = LoggerFactory.Create(l =>
+        {
+            l.AddProvider(new XunitLoggerProvider(testOutputHelper));
+        });
+    }
+
     [Fact]
     public async Task TestViaGrpcChannel()
     {
@@ -31,13 +43,25 @@ public class GreeterServiceTests
     [Fact]
     public async Task TestViaCodeFirst()
     {
-        using var webApp = new WebApplicationFactory<Program>();
+        using var webApp = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.AddSingleton(_loggerFactory);
+                });
+            });
 
         var services = new ServiceCollection();
         services.AddCodeFirstGrpcClient<IGreeterService>(o =>
         {
             o.Address = webApp.Server.BaseAddress;
+        })
+        .ConfigureChannel(channelOptions =>
+        {
+            channelOptions.LoggerFactory = _loggerFactory;
         });
+
         var serviceProvider = services.BuildServiceProvider();
         var factory = serviceProvider.GetRequiredService<Gncf.GrpcClientFactory>();
         var client = factory.CreateClient<IGreeterService>(nameof(IGreeterService));
